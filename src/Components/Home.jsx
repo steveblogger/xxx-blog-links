2d17e6c02efd10bdc8c853f90a97ff087011bea4
steveblogger <sr0824895@gmail.com>
2023-04-29 23:37:27 +0530

    commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Data } from "./Utilities/Links"
 
 function Home() {
+  const [page, setPage] = useState(1)
+  const [pageData, setPageData] = useState(Data.slice(0, 10))
+
+  function jumpButtons() {
+    var links = Data.length
+    var next = (links - (10 * (page - 1))) <= 10
+    console.log(page);
+    return <div className='nav-buttons'>
+      {page <= 1 ? <span></span> : <button className='prev-button' onClick={prevPage}>Previous</button>}
+      {next ? <span></span> : <button className='next-button' onClick={nextPage}>Next</button>}
+    </div>
+  }
+
+  function prevPage() {
+    var start = (page - 2) * 10
+    setPage(page - 1)
+    if ((Data.length - start) < 10) {
+      setPageData(Data.slice(start))
+    } else {
+      setPageData(Data.slice(start, start + 10))
+    }
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  function nextPage() {
+    var start = page * 10
+    setPage(page + 1)
+    if ((Data.length - start) < 10) {
+      setPageData(Data.slice(start))
+    } else {
+      setPageData(Data.slice(start, start + 10))
+    }
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+
   return (
-    <div>Home</div>
+    <div className='box'>
+      {pageData.map((content) => {
+        return content
+      })
+      }
+      {jumpButtons()}
+    </div>
   )
 }
 
